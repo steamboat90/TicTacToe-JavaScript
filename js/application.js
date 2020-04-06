3e65029d53b24c428b911e4519da6e7c2b7c53ba
jkl3699 <31252127+steamboat90@users.noreply.github.com>
2020-04-06 12:45:34 -0500

    Made the Grid and Title

--- js/application.js
+++ js/application.js
@@ -1,4 +1,12 @@
-function changeText(){
-    document.getElementById("p1").innerHTML = "Tic Tac Toe";
-   }
-   changeText();
+
+function changeText(text){
+    document.getElementById("p1").innerHTML = text;
+    document.getElementsByClassName("grid-item").addEventListener("mouseover", alert());
+}
+
+function addClickListener(){
+    //document.getElementById("top").addEventListener("mouseover", changeText(4));
+    document.getElementsByClassName("grid-item").addEventListener("onclick", alert());
+}
+
+changeText("Tic Tac Toe");
